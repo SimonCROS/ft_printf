770143bf85e59e5b8d072a77b9d5877ae5ca0824
Simon Cros <scros@student.42lyon.fr>
2020-12-15 18:43:35 +0100

    Bugged support for d and i (some changes nessaries)

--- src/ft_applyer.c
+++ src/ft_applyer.c
@@ -6,13 +6,13 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 15:14:52 by scros             #+#    #+#             */
-/*   Updated: 2020/12/15 16:49:26 by scros            ###   ########lyon.fr   */
+/*   Updated: 2020/12/15 18:41:45 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	final_len(t_modifiers para, char *s)
+static int	final_str_len(t_modifiers para, char *s)
 {
 	int len;
 
@@ -23,6 +23,11 @@ static int	final_len(t_modifiers para, char *s)
 	else
 		len = ft_strlen(s);
 	return (ft_max(para.min_width, len));
+}
+
+static int	prec_int_len(t_modifiers para, int i)
+{
+	return (ft_max(ft_intlen(i), ft_abs(para.prec)));
 }
 
 static int	char_type(t_modifiers para, char c)
@@ -41,7 +46,7 @@ static int	string_type(t_modifiers para, char *s)
 	int		len;
 	int		from;
 	int		s_len;
-	char	str[final_len(para, s)];
+	char	str[final_str_len(para, s)];
 
 	if (!s)
 		return (string_type(para, "(null)"));
@@ -56,6 +61,24 @@ static int	string_type(t_modifiers para, char *s)
 			from);
 	}
 	return (write(1, str, len));
+}
+
+static int	int_type(t_modifiers para, int i)
+{
+	int		len;
+	char	i_str[ft_intlen(i)];
+	char	str[prec_int_len(para, i) + 1];
+
+	len = sizeof(str) - 1;
+	str[len] = 0;
+	ft_memset(&str, ft_ternary(para.prec < 0, ' ', '0'), len);
+	ft_itoa_to(i, i_str);
+	ft_strinsert(str, i_str, ft_ternary(ft_max(para.flags.left_align,
+		para.prec < 0), 0, len - sizeof(i_str)));
+	if (para.has_prec)
+		para.flags.zero = 0;
+	para.has_prec = 0;
+	return (string_type(para, str));
 }
 
 static int	pointer_type(t_modifiers para, void *p)
@@ -93,5 +116,7 @@ int			ft_applyer(t_modifiers para, va_list args)
 		return (string_type(para, va_arg(args, char*)));
 	else if (para.type == 'p')
 		return (pointer_type(para, va_arg(args, void*)));
+	else if (para.type == 'd' || para.type == 'i')
+		return (int_type(para, va_arg(args, int)));
 	return (0);
 }
